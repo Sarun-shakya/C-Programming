1a42c3c0ec8048bd9c0012fce644f39f8455e9c1
Sarun-shakya <sarunshakya414@gmail.com>
2024-07-28 23:07:11 +0545

    file added:

---
+++ Basic Programs/ASCII_Char.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main(){
+    char ch;
+    printf("enter any character: ");
+    scanf("%c",&ch);
+    printf("the ASCII value of character %c is %d",ch,ch);
+    return 0;
+}
